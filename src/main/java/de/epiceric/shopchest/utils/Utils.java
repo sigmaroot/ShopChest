fa6814b8d409181a4fb2f8a7581cc1c1acfad213
Steven M <admin@sigmaroot.de>
2017-11-22 19:27:59 +0100

    Fix books changing metadata by serialization

--- src/main/java/de/epiceric/shopchest/utils/Utils.java
+++ src/main/java/de/epiceric/shopchest/utils/Utils.java
@@ -58,6 +58,8 @@ public class Utils {
 
             itemStack1.setItemMeta(bookMeta1);
             itemStack2.setItemMeta(bookMeta2);
+
+            itemStack1 = decode(encode(itemStack1));
         }
 
         return itemStack1.isSimilar(itemStack2);
